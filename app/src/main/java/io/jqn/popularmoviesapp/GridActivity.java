7df45eacd4f7acc577c47aa71877c1f7bee33f99
jQN <jqn@jQNs-MacBook-Pro.local>
2018-06-15 17:09:15 -0600

    Added single image with picasso

---
+++ app/src/main/java/io/jqn/popularmoviesapp/GridActivity.java
@@ -0,0 +1,15 @@
+package io.jqn.popularmoviesapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GridActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_grid);
+    }
+
+//    TODO fetch movies from movies API
+}
--- app/src/main/java/io/jqn/popularmoviesapp/MainActivity.java
+++ app/src/main/java/io/jqn/popularmoviesapp/MainActivity.java
@@ -2,6 +2,9 @@ package io.jqn.popularmoviesapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // get a reference to the ImageView
+        ImageView imageView = findViewById(R.id.imageCell);
+        // load an image into the ImageView
+        Picasso.get().load("http://i.imgur.com/DvpvklR.png").into(imageView);
     }
 }
